6ae3e1a28ab0b01726b2d2996fb3ec843ccbb6cf
amartel2025 <antonin.mrtl.pr@gmail.com>
2026-03-23 16:45:33 +0100

    Fix MainController Change name template Connect BDD

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -8,11 +8,9 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class MainController extends AbstractController
 {
-    #[Route('/main', name: 'app_main')]
+    #[Route('', name: 'main_home')]
     public function index(): Response
     {
-        return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
-        ]);
+        return $this->render('main/home.html.twig');
     }
 }
