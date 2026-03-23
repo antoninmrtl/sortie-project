3b1f5fd3ac3664b1e0412b57dcc622197340e3e1
antonin martel <antonin.mrtl.pr@gmail.com>
2026-03-23 21:17:10 +0100

    fix name home and add repository for test Test display info in home

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\QuestRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,8 +11,12 @@ use Symfony\Component\Routing\Attribute\Route;
 final class MainController extends AbstractController
 {
     #[Route('', name: 'main_home')]
-    public function index(): Response
+    public function home(QuestRepository $questRepository): Response
     {
-        return $this->render('main/home.html.twig');
+
+        $qests = $questRepository->findAll();
+        return $this->render('main/home.html.twig', [
+            'quests' => $qests,
+        ]);
     }
 }
