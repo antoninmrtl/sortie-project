34e018535a5be2ff6d0f6b2edf72ef74727c88ea
amartel2025 <antonin.mrtl.pr@gmail.com>
2026-03-31 11:58:59 +0200

    Update fixtures

--- src/DataFixtures/QuestFixtures.php
+++ src/DataFixtures/QuestFixtures.php
@@ -20,34 +20,90 @@ class QuestFixtures extends Fixture implements DependentFixtureInterface
     public const QUEST_REFERENCE = 'quest';
 
 
+//    public function load(ObjectManager $manager): void
+//    {
+//
+//        $faker = Factory::create('fr-FR');
+//
+//        for ($i = 0; $i < 50; $i++) {
+//            $quest = new Quest();
+//            $quest->setName($faker->text(25))
+//                ->setDuration($faker->randomFloat(2, 1, 24))
+//                ->setInfoQuest($faker->text(200))
+//                ->setStartDateTime($faker->dateTimeBetween('-5 month'));
+//            $quest->setInscriptionLimitDate($faker->dateTimeBetween($quest->getStartDateTime(), '+1 years'))
+//                ->setStatus($this->getReference(StatusFixtures::STATUS_REFERENCE, Status::class))
+//                ->setNbMaxInscription($faker->numberBetween(5, 60))
+//                ->setCampus($this->getReference(CampusFixtures::CAMPUS_REFERENCE, Campus::class))
+//                ->setPlace($this->getReference(PlaceFixtures::PLACE_REFERENCE, Place::class))
+//                ->setPromoter($this->getReference(UserFixtures::USER_REFERENCE, User::class));
+//
+//            $manager->persist($quest);
+//
+//        }
+//
+//
+//        $manager->flush();
+//
+//        $this->addReference(self::QUEST_REFERENCE, $quest);
+//    }
+
+
     public function load(ObjectManager $manager): void
     {
-
         $faker = Factory::create('fr-FR');
 
-        for ($i = 0; $i < 50; $i++) {
+        $allStatuses = $manager->getRepository(Status::class)->findAll();
+        $statusMap = [];
+        foreach ($allStatuses as $s) { $statusMap[$s->getLabel()] = $s; }
+
+        for ($i = 0; $i < 60; $i++) {
             $quest = new Quest();
-            $quest->setName($faker->text(25))
-                ->setDuration($faker->randomFloat([2], [1], [24])) //bug sur la duration en bdd
-                ->setInfoQuest($faker->text(200))
-                ->setStartDateTime($faker->dateTimeBetween('-5 month'));
-            $quest->setInscriptionLimitDate($faker->dateTimeBetween($quest->getStartDateTime(), '+1 years'))
-                ->setStatus($this->getReference(StatusFixtures::STATUS_REFERENCE, Status::class))
-                ->setNbMaxInscription($faker->numberBetween(5, 60))
+
+            if ($i < 10) { // CAS ARCHIVE (Il y a plus de 30 jours)
+                $start = $faker->dateTimeBetween('-60 days', '-31 days');
+            } elseif ($i < 20) { ///CAS PASSÉE (Finie mais moins de 30 jours)
+                $start = $faker->dateTimeBetween('-30 days', '-5 days');
+            } elseif ($i < 25) { // CAS EN COURS (Commencée il y a 1h)
+                $start = new \DateTime('-1 hour');
+            } elseif ($i < 40) { // CAS OUVERTE (Dans le futur)
+                $start = $faker->dateTimeBetween('+5 days', '+30 days');
+            } else { // CAS CLÔTURÉE (Demain, mais date limite d'inscription dépassée)
+                $start = new \DateTime('+2 days');
+            }
+
+            $quest->setName($faker->realText(25))
+                ->setDuration($faker->randomFloat(2, 1, 5)) // Durée courte pour faciliter les tests
+                ->setInfoQuest($faker->realText(200))
+                ->setStartDateTime($start);
+
+
+            if ($i >= 40) {
+                $quest->setInscriptionLimitDate(new \DateTime('-1 day'));
+            } else {
+                $quest->setInscriptionLimitDate($faker->dateTimeBetween('-2 months', $quest->getStartDateTime()->format('Y-m-d H:i:s')));            }
+
+
+            if ($i === 0) {
+                $quest->setStatus($statusMap['Annulée']);
+            } elseif ($i === 1) {
+                $quest->setStatus($statusMap['En création']);
+            } else {
+                $quest->setStatus($statusMap['Ouverte']);
+            }
+
+            $quest->setNbMaxInscription($faker->numberBetween(5, 60))
                 ->setCampus($this->getReference(CampusFixtures::CAMPUS_REFERENCE, Campus::class))
                 ->setPlace($this->getReference(PlaceFixtures::PLACE_REFERENCE, Place::class))
-                ->setPromoter($this->getReference(UserFixtures::USER_REFERENCE, User::class));
+               ->setPromoter($this->getReference(UserFixtures::USER_REFERENCE, User::class));
 
             $manager->persist($quest);
-
         }
 
-
         $manager->flush();
-
         $this->addReference(self::QUEST_REFERENCE, $quest);
-    }
 
+    }
     public function getDependencies(): array
     {
         return [
